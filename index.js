945e6055e405ee3f61f110313b336dfc56864c9b
Omer <omer-meraloglu@hotmail.com>
2022-09-22 17:02:28 +0300

    integrated API

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-// https://www.omdbapi.com/?t=Star+Wars&apikey=335df740
-const movieList = document.querySelector(".movies")
+// Titles: https://www.omdbapi.com/?t=thor&page=1&apikey=335df740
+// Details: http://www.omdbapi.com/?apikey=[yourkey]&
 
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('movies');
 
-async function main() {
-    const movie = await fetch("https://www.omdbapi.com/?t=Star+Wars&apikey=335df740");
-    const movieData = await movie.json();
+movieSearchBox.addEventListener("input", e => {
+    const value = e.target.value
+    console.log(value)
+    loadMovies(value)
+    
+})
 
+//load movies from API
+async function loadMovies(searchItem){
+    const URL = `https://www.omdbapi.com/?t=${searchItem}&page=1&apikey=335df740`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
 
-
-    movieList.innerHTML = movieData.map((user) => ` 
-    <div class="movie">
-    <figure class="book__img--wrapper">
-        <img class="movie__img"
-            src="https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
-            alt="">
-    </figure>
-    <div class="movie__title">Fast And The Furious</div>
-    <div class="movie__ratings">IMDB Rating: </div>
-</div>
-`)
-        .join("");
-
+    if(data.Response == "True") displayMovieDetails(data);
 
 }
 
-main();
+
+function displayMovieDetails(movieData){ 
+    searchList.innerHTML = `<div class="movie">
+    <figure class="book__img--wrapper">
+        <img class="movie__img"
+            src="${movieData.Poster}"
+            alt="">
+    </figure>
+    <div class="movie__title">${movieData.Title}</div>
+    <div class="movie__ratings">IMDB Rating: ${movieData.imdbRating} </div>
+    
+</div>
+<div class="more__info"> Year: ${movieData.Year} <br> Genre: ${movieData.Genre} <br> Runtime: ${movieData.Runtime} <br>
+${movieData.Plot}
+</div>`
+}
+
+
+
+
